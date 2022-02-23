cf9e1cd71d53adf6de033d640848782d037c41b9
Kinsey Johnson <yesnikjohnson@gmail.com>
2022-02-22 21:32:34 -0500

    quote already display, and change quote function

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-//grabbing button by finding the id
+//dispay quote
+
 window.onload=function(){
+let quoteHere = document.getElementById("quoteDisplay")
+
+fetch('https://api.kanye.rest')
+.then(res => res.json())
+.then(quote => {
+    console.log(quote)
+    quoteHere.innerHTML = `<p> ${quote.quote} -kanye </p> <a class="twitter-share-button"
+    href="https://twitter.com/intent/tweet?text=${quote.quote} -kanye" target="_blank">
+    tweet it</a>`
+})  
+
+//new quote
+
 let quoteButton = document.getElementById("button")
 
 quoteButton.addEventListener("click", e => {
 
-    let quoteDiv = document.getElementById("quoteHere")
+    // let quoteDiv = document.getElementById("quoteDisplay")
 
     fetch('https://api.kanye.rest')
+    
     .then(res => res.json())
     .then(quote => {
         console.log(quote)
-        quoteDiv.innerHTML = `<p>"${quote.quote}" -kanye</p> <br> <a class="twitter-share-button"
+        quoteHere.innerHTML = `<p>"${quote.quote}" -kanye <a class="twitter-share-button"
         href="https://twitter.com/intent/tweet?text=${quote.quote} -kanye" target="_blank">
-        Tweet</a>`
+        tweet it</a></p>`
         })
         });
-}
+    }
